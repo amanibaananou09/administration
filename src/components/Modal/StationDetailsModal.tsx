e0deca6b4b181de22eb81c9546612f76d95de33a
Akram MONCER <akram.moncer@gmail.com>
2024-01-03 15:52:05 +0100

    refactoring detail modal

--- src/components/Modal/StationDetailsModal.tsx
+++ src/components/Modal/StationDetailsModal.tsx
@@ -1,18 +1,7 @@
-import React from "react";
-import {
-  Button,
-  Flex,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalHeader,
-  ModalOverlay,
-  Text,
-} from "@chakra-ui/react";
-import { GeneralStations } from "../../common/AdminModel";
-import { ModalFooter } from "@chakra-ui/modal";
+import { Flex, Text } from "@chakra-ui/react";
+import UIDetailModal from "components/UI/Modal/UIDetailModal";
 import { useTranslation } from "react-i18next";
+import { GeneralStations } from "../../common/AdminModel";
 
 interface StationDetailsModalProps {
   isOpen: boolean;
@@ -28,92 +17,74 @@ const StationDetailsModal = ({
   const { t } = useTranslation();
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} size="xl">
-      <ModalOverlay />
-      <ModalContent
-        style={{
-          backgroundColor: "gray.100",
-          borderRadius: "10px",
-        }}
-      >
-        <ModalHeader
-          style={{
-            backgroundColor: "black",
-            color: "white",
-          }}
-        >
-          {t("stationDetailsModel.header")}
-        </ModalHeader>
-        <ModalCloseButton color="white" />
-        <ModalBody>
-          {stationDetails && (
-            <Flex justifyContent="space-between" flexWrap="wrap" p="4">
-              <div style={{ marginBottom: "10px" }}>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.name")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.creatorAccount")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.customerAccount")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.controllerPts")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.controllerType")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.phone")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.address")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.country")}
-                </Text>
-                <Text fontWeight="bold" marginBottom="5px">
-                  {t("stationDetailsModel.gps")}
-                </Text>
-              </div>
-              <div>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.name ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.creatorCustomerAccountName ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.customerAccountName ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.controllerPts.ptsId ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.controllerPts.controllerType ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.controllerPts.phone ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.address ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.country.name ?? "-"}
-                </Text>
-                <Text fontWeight="normal" marginBottom="5px">
-                  {stationDetails.cordonneesGps ?? "-"}
-                </Text>
-              </div>
-            </Flex>
-          )}
-        </ModalBody>
-        <ModalFooter>
-          <Button onClick={onClose}>Close</Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+    <UIDetailModal
+      title={t("stationDetailsModel.header")}
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      {stationDetails && (
+        <Flex justifyContent="space-between" flexWrap="wrap" p="4">
+          <div style={{ marginBottom: "10px" }}>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.name")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.creatorAccount")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.customerAccount")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.controllerPts")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.controllerType")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.phone")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.address")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.country")}
+            </Text>
+            <Text fontWeight="bold" marginBottom="5px">
+              {t("stationDetailsModel.gps")}
+            </Text>
+          </div>
+          <div>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.name ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.creatorCustomerAccountName ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.customerAccountName ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.controllerPts.ptsId ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.controllerPts.controllerType ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.controllerPts.phone ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.address ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.country.name ?? "-"}
+            </Text>
+            <Text fontWeight="normal" marginBottom="5px">
+              {stationDetails.cordonneesGps ?? "-"}
+            </Text>
+          </div>
+        </Flex>
+      )}
+    </UIDetailModal>
   );
 };
 
